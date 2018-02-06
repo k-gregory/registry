0101424f082406418c5e84f38d861ce883a31b03
Gregory K <xalvevs@gmail.com>
2018-02-06 22:46:36 +0200

    Add comment form

--- src/main/java/io/github/k_gregory/registry/controller/IndexController.java
+++ src/main/java/io/github/k_gregory/registry/controller/IndexController.java
@@ -4,11 +4,11 @@ package io.github.k_gregory.registry.controller;
 import io.github.k_gregory.registry.model.Comment;
 import io.github.k_gregory.registry.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class IndexController {
     private final CommentRepository comments;
 
@@ -17,13 +17,20 @@ public class IndexController {
         this.comments = comments;
     }
 
-    @RequestMapping("/")
-    @ResponseBody
-    Iterable<Comment> index(){
-        Comment comment = new Comment();
-        comment.value = "Ololo";
-        comments.save(comment);
-
+    @ModelAttribute("comments")
+    Iterable<Comment> modelComments(){
         return comments.findAll();
+    }
+
+    @PostMapping("/")
+    String addComment(Model model, Comment comment){
+        comments.save(comment);
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    String index(Model model){
+        model.addAttribute("comment", new Comment());
+        return "index";
     }
 }
--- src/main/java/io/github/k_gregory/registry/model/Comment.java
+++ src/main/java/io/github/k_gregory/registry/model/Comment.java
@@ -5,10 +5,25 @@ import javax.persistence.*;
 @Entity
 @Table(name = "comment")
 public class Comment {
+    private Integer id;
+    private String value;
+
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Id @Column(name = "id")
-    public Integer id;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     @Column(name = "value")
-    public String value;
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
