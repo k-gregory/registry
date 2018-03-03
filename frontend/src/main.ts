be440c2fc9de8089a99a5c238d4411b5689864d4
Kostyantyn Sharovarsky <kostya9@gmail.com>
2018-03-04 01:08:55 +0200

    Add landing page boilerplate

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Buefy from 'buefy';
+import 'buefy/lib/buefy.css';
+import 'mdi/css/materialdesignicons.css';
 
+Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav class="level toolbar box">
+      <div class="level-item">
+        <b-field>
+          <b-input placeholder="Знайти..."
+              type="search"
+              icon="magnify">
+          </b-input>
+          <p class="control">
+              <button class="button is-primary">Пошук</button>
+          </p>
+        </b-field>
+      </div>
+      <div class="level-item">
+        <div class="title">Автоматизована система виконавчого провадження</div>
+      </div>
+      <div class="level-item">
+        <button class="button is-primary">Увійти</button>
+      </div>
+    </nav>
+    <section class="search">
+      <div class="container">
+        <LandingSearchTable>
+        </LandingSearchTable>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import LandingSearchTable from '@/components/LandingSearchTable.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    LandingSearchTable,
   },
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+
+.search {
+  padding-top: 50px;
+}
+
+.toolbar {
+  padding: 20px 0;
+}
+
+</style>
