eb26f5a305bb89b2865c1dc1ee955287277f7dc6
Gregory K <xalvevs@gmail.com>
2018-03-04 23:56:03 +0200

    Repository must not return DTO

--- backend/src/main/java/io/github/k_gregory/registry/repository/EnforcementRepository.java
+++ backend/src/main/java/io/github/k_gregory/registry/repository/EnforcementRepository.java
@@ -1,6 +1,5 @@
 package io.github.k_gregory.registry.repository;
 
-import io.github.k_gregory.registry.dto.TopEnforcementDTO;
 import io.github.k_gregory.registry.model.Enforcement;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,8 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface EnforcementRepository extends JpaRepository<Enforcement, Long> {
-    @Query("select new io.github.k_gregory.registry.dto.TopEnforcementDTO(" +
-            "e.id, e.sender, e.receiver, f.name, e.startedAt, e.state" +
-            ") from Enforcement e join e.facility f")
-    List<TopEnforcementDTO> findTop(Pageable p);
+    @Query("select e from Enforcement e join fetch e.facility")
+    List<Enforcement> fetchAllWithFacility(Pageable p);
 }
--- backend/src/main/java/io/github/k_gregory/registry/service/impl/EnforcementServiceImpl.java
+++ backend/src/main/java/io/github/k_gregory/registry/service/impl/EnforcementServiceImpl.java
@@ -3,24 +3,39 @@ package io.github.k_gregory.registry.service.impl;
 import io.github.k_gregory.registry.dto.TopEnforcementDTO;
 import io.github.k_gregory.registry.repository.EnforcementRepository;
 import io.github.k_gregory.registry.service.EnforcementService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EnforcementServiceImpl implements EnforcementService {
     private final EnforcementRepository enforcements;
+    private final ModelMapper mapper;
 
     @Autowired
-    public EnforcementServiceImpl(EnforcementRepository enforcements) {
+    public EnforcementServiceImpl(EnforcementRepository enforcements, ModelMapper mapper) {
         this.enforcements = enforcements;
+        this.mapper = mapper;
     }
 
     @Override
     public List<TopEnforcementDTO> listTopEnforcements() {
-        return enforcements.findTop(PageRequest.of(0, 10, Sort.by("id")));
+        return this.enforcements
+                .fetchAllWithFacility(PageRequest.of(0, 10, Sort.by("id")))
+                .stream()
+                .map(e -> new TopEnforcementDTO(
+                        e.getId(),
+                        e.getSender(),
+                        e.getReceiver(),
+                        e.getFacility().getName(),
+                        e.getStartedAt(),
+                        e.getState()
+                ))
+                .collect(Collectors.toList());
     }
 }
