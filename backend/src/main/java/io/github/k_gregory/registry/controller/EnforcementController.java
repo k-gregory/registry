e1611e54156ee3ff121c6d7766b7dfdd8f1f8298
Gregory K <xalvevs@gmail.com>
2018-03-04 13:00:36 +0200

    Rename 'solution' table to 'enforcement'

---
+++ backend/src/main/java/io/github/k_gregory/registry/controller/EnforcementController.java
@@ -0,0 +1,9 @@
+package io.github.k_gregory.registry.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/enforcemet")
+public class EnforcementController {
+}
