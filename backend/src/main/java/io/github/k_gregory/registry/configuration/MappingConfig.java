1dcfb495ac1e92dc70bcd9e3d788561c8cb7d5f5
Gregory K <xalvevs@gmail.com>
2018-03-05 02:13:17 +0200

    Fix ModelMapper configuration

--- backend/src/main/java/io/github/k_gregory/registry/configuration/MappingConfig.java
+++ backend/src/main/java/io/github/k_gregory/registry/configuration/MappingConfig.java
@@ -8,16 +8,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 @Configuration
 public class MappingConfig {
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+    private final Optional<List<MapperConfigurer>> mapperConfigurers;
 
     @Autowired
-    public static void mapTopEnforcementDTO(ModelMapper mapper){
-        TypeMap<Enforcement, TopEnforcementDTO> map = mapper.createTypeMap(Enforcement.class, TopEnforcementDTO.class);
-        map.addMapping(src->src.getFacility().getName(), TopEnforcementDTO::setFacilityName);
+    public MappingConfig(Optional<List<MapperConfigurer>> mapperConfigurers) {
+        this.mapperConfigurers = mapperConfigurers;
     }
+
+    @FunctionalInterface
+    public interface MapperConfigurer {
+        void configure(ModelMapper mapper);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapperConfigurers
+                .orElseGet(Collections::emptyList)
+                .forEach(c -> c.configure(mapper));
+        return mapper;
+    }
+
+    @Bean
+    public static MapperConfigurer mapTopEnforcementDTO() {
+        return (mapper) -> {
+            TypeMap<Enforcement, TopEnforcementDTO> map = mapper.createTypeMap(
+                    Enforcement.class,
+                    TopEnforcementDTO.class
+            );
+
+            map.addMapping(
+                    src -> src.getFacility().getName(),
+                    TopEnforcementDTO::setFacilityName
+            );
+        };
+    }
+
 }
